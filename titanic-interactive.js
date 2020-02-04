0f46e51446087c5ec61e4ebf08fb45be7dfc261e
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-02-03 22:20:27 -0800

    change button to reset

--- titanic-interactive.js
+++ titanic-interactive.js
@@ -17,29 +17,25 @@ fetch('titanic-passengers.json')
 
       function handleData(data) {
         const fields = data.map(passengers => passengers.fields)
-        console.log(data);
-        genderGraph(fields)
-      }
+        
+        fields.forEach(passenger => {
+          const el = document.createElement('div')
+          passengerDiv.appendChild(el)
+          elements.push(el)             // store the element
+          passengerData.push(passenger) // Store the passenger
+      
+          el.style.width = '14px'
+          el.style.height = '14px'
+          el.style.backgroundColor = 'gray'
+          el.style.margin = '1.5px'
+          el.style.transition = '3s'
+          el.style.boxSizing = 'border-box'
+        });
 
-      // Handle interaction
+        // console.log(data);
+      }
      
       // console.log(showGender)
-
-
-      function genderGraph (fields) {
-        fields.forEach(passenger => {
-          //make an element
-          const el = document.createElement('div')
-          //attach the element to the dom 
-          gender.appendChild(el)
-          //styling element 
-          el.style.width = '10px'
-          el.style.height = '10px'
-          el.style.margin = '1px'
-          el.style.backgroundColor = 'gray'
-          el.style.transition = '2s'
-        }) 
-      }
 
       buttonGender.addEventListener('click', (e) => {
         showGender = !showGender
@@ -51,6 +47,7 @@ fetch('titanic-passengers.json')
           passengerData.forEach((passenger, i) => {
             elements[i].style.backgroundColor = passenger.sex === 'male' ? '#0000ff' : '#ff007f'
           })
+          document.getElementById('show-gender').innerHTML = 'Reset'
         } else {
           e.target.style.backgroundColor = 'black'
           e.target.style.color = 'white'
@@ -59,41 +56,17 @@ fetch('titanic-passengers.json')
           passengerData.forEach((passenger, i) => {
             elements[i].style.backgroundColor = "gray"
           })
+          document.getElementById('show-gender').innerHTML = 'Show Gender'
         }
       })
 
-      function selectButton(el, state) {
-        if (state == true) {
-          if (el.sex === 'male') {
-            el.style.backgroundColor = 'blue'
-          } else {
-            el.style.backgroundColor = 'pink'
-          }
-        } 
-      }
+      // function selectButton(el, state) {
+      //   if (state == true) {
+      //     if (el.sex === 'male') {
+      //       el.style.backgroundColor = 'blue'
+      //     } else {
+      //       el.style.backgroundColor = 'pink'
+      //     }
+      //   } 
+      // }
 
-function handleData(data) {
-  const fields = data.map(({ fields }) => fields)
-
-  fields.forEach(passenger => {
-    const el = document.createElement('div')
-    passengerDiv.appendChild(el)
-    elements.push(el)             // store the element
-    passengerData.push(passenger) // Store the passenger
-
-    el.style.width = '14px'
-    el.style.height = '14px'
-    el.style.backgroundColor = 'gray'
-    el.style.margin = '1px'
-    el.style.transition = '200ms'
-    el.style.boxSizing = 'border-box'
-  });
-}
-
-function displayByGender() {
-  passengerData.forEach((obj, i) => {
-    const el = elements[i]
-    const color = obj.sex === 'male' ? 'blue' : 'pink'
-    el.style.backgroundColor = showGender ? color : 'white'
-  })
-}
